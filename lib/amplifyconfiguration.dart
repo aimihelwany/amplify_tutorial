6ec90e6f864fcfccd1cedafaff661ae4bff6bb3a
aimihelwany <aimihelwany@gmail.com>
2021-01-02 22:52:44 +0800

    first commit

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:799db9d8-9115-46f0-adc1-51ba1313f9c7",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_jKPwvpmT9",
+                        "AppClientId": "23pkvtlt1vbb4grbspsp63ehv8",
+                        "AppClientSecret": "qlvm8l2eumtbvq18lh3pq101dohphl5iuvae2l395df2c8fqveh",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
